9c4de9536d86d08cde0878df855452338201a26d
kremir <kremir@o2.pl>
2019-12-18 22:57:57 +0100

    display list of object - thymeleaf

--- src/main/java/com/example/mk/thymeleafhello/CarController.java
+++ src/main/java/com/example/mk/thymeleafhello/CarController.java
@@ -4,14 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
     @GetMapping("/car")
     public String get(Model model)
     {
-        Car car =new Car("Skoda", "Fabia");
-        model.addAttribute("car", car);
+        Car car1 =new Car("Fabia", "Skoda");
+        Car car2 =new Car("Fusion", "Ford");
+        Car car3 =new Car("RX8", "Mazda");
+        Car car4 =new Car("Seicento", "Fiat");
+
+        List<Car> list = Arrays.asList(car1, car2, car3, car4);
+        model.addAttribute("cars", list);
         return "car";
     }
 }
